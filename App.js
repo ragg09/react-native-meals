bed40d495b821a204fd6a20ae00159b74e8cb3ca
unknown <ragunayon@gmail.com>
2023-08-09 16:37:40 +0800

    Setup React Navigation

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Fragment } from 'react';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View>
+    <Fragment>
       <StatusBar style="light" />
-      <CategoriesScreen />
-    </View>
+
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Meals" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Fragment>
   );
 }
 
