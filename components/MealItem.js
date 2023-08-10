f8868b758fb37e9bc81d788a19ef9a4a1f90e239
unknown <ragunayon@gmail.com>
2023-08-10 13:21:18 +0800

    Display Meals on click

---
+++ components/MealItem.js
@@ -0,0 +1,11 @@
+import { Pressable, Text, View, StyleSheet, Platform } from 'react-native';
+
+export default MealItem = ({ title }) => {
+  return (
+    <View>
+      <Text>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
--- screens/MealsOverView.js
+++ screens/MealsOverView.js
@@ -1,12 +1,25 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { FlatList, View, StyleSheet } from 'react-native';
 import { MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 export default MealsOverView = ({ route }) => {
   const catId = route.params.categoryId;
 
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  const renderMealItem = ({ item }) => {
+    return <MealItem title={item.title} />;
+  };
+
   return (
     <View style={styles.container}>
-      <Text>MEALS OVERVIEW SCREEN {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
