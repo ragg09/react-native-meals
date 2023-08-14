dec5e7a27f3fe45f9aadc9b9934b8e9338a76198
unknown <ragunayon@gmail.com>
2023-08-14 14:02:41 +0800

    Nested Navigation

--- App.js
+++ App.js
@@ -1,17 +1,60 @@
+import 'react-native-gesture-handler';
 import { Button, StyleSheet, Text } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Fragment } from 'react';
+import { Ionicons } from '@expo/vector-icons';
 import MealsOverView from './screens/MealsOverView';
 import MealDetailScreen from './screens/MealDetailScreen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import FavoritesScreen from './screens/FavoritesScreen';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
+  const Drawernavigator = () => {
+    return (
+      <Drawer.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#351401'
+          },
+          headerTintColor: 'white',
+          sceneContainerStyle: { backgroundColor: '#3f2f25' },
+          drawerContentStyle: {
+            backgroundColor: '#351401'
+          },
+          drawerInactiveTintColor: 'white',
+          drawerActiveTintColor: '#3f2f25',
+          drawerActiveBackgroundColor: '#e4baa1'
+        }}
+      >
+        <Drawer.Screen
+          name="Categories"
+          component={CategoriesScreen}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons color={color} size={size} name="list" />
+            )
+          }}
+        />
+        <Drawer.Screen
+          name="Favorites"
+          component={FavoritesScreen}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons color={color} size={size} name="star" />
+            )
+          }}
+        />
+      </Drawer.Navigator>
+    );
+  };
+
   return (
-    <Fragment>
+    <>
       <StatusBar style="inverted" />
 
       <NavigationContainer>
@@ -27,9 +70,10 @@ export default function App() {
         >
           <Stack.Screen
             name="Meals"
-            component={CategoriesScreen}
+            component={Drawernavigator}
             options={{
-              title: 'Home'
+              title: 'Home',
+              headerShown: false
             }}
           />
           <Stack.Screen name="MealsOverview" component={MealsOverView} />
@@ -42,7 +86,7 @@ export default function App() {
           />
         </Stack.Navigator>
       </NavigationContainer>
-    </Fragment>
+    </>
   );
 }
 
